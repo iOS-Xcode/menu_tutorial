f5a243b607c91b667b7e0e02afcfdcec863eb9be
Seokhyun Kim <wjapp72@gmail.com>
2021-03-06 10:05:37 -0800

    Initial Commit

---
+++ menu_tutorial/menu_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  menu_tutorialApp.swift
+//  menu_tutorial
+//
+//  Created by Seokhyun Kim on 2021-03-06.
+//
+
+import SwiftUI
+
+@main
+struct menu_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
